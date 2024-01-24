0b359cde88f0307249c89830e963ce284326e4fd
Pascal Berrang <git@paberr.net>
2024-01-24 12:06:25 -0600

    Use correct circuits

--- phase2-cli/src/bin/create_nimiq_circuits.rs
+++ phase2-cli/src/bin/create_nimiq_circuits.rs
@@ -1,7 +1,7 @@
 use ark_ec::pairing::Pairing;
 use ark_mnt4_753::MNT4_753;
 use ark_mnt6_753::MNT6_753;
-use ark_relations::r1cs::ConstraintSynthesizer;
+use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystem};
 use ark_serialize::CanonicalSerialize;
 use ark_std::UniformRand;
 use nimiq_zkp_circuits::circuits::{
@@ -17,7 +17,16 @@ use tracing_subscriber::{
     fmt::{time::ChronoUtc, Subscriber},
 };
 
-fn create_circuit<E: Pairing, C: ConstraintSynthesizer<E::ScalarField>>(circuit: C, filename: &str) {
+fn create_circuit<E: Pairing, C: ConstraintSynthesizer<E::ScalarField> + Clone>(circuit: C, filename: &str) {
+    // Test circuit first
+    let cs = ConstraintSystem::new_ref();
+    // Synthesize the circuit.
+    circuit
+        .clone()
+        .generate_constraints(cs.clone())
+        .expect("constraint generation should not fail");
+    assert!(cs.is_satisfied().unwrap());
+
     let cs = circuit_to_qap::<E, _>(circuit).expect("Could not prepare circuit for QAP");
 
     let matrices = cs.to_matrices().expect("Could not generate matrices");
@@ -33,18 +42,18 @@ fn create_circuit<E: Pairing, C: ConstraintSynthesizer<E::ScalarField>>(circuit:
 
 fn create_circuits() {
     let rng = &mut thread_rng();
-    create_circuit::<MNT6_753, _>(MergerWrapperCircuit::rand(rng), "merger_wrapper");
-    create_circuit::<MNT4_753, _>(MergerCircuit::rand(rng), "merger");
-    create_circuit::<MNT6_753, _>(MacroBlockWrapperCircuit::rand(rng), "macro_block_wrapper");
-    create_circuit::<MNT4_753, _>(MacroBlockCircuit::rand(rng), "macro_block");
-    for tree_level in 0..5 {
-        let filename = format!("pk_tree_{}", tree_level);
-        if tree_level % 2 == 0 {
-            create_circuit::<MNT6_753, _>(MNT4PKTreeNodeCircuit::rand(tree_level, rng), &filename);
-        } else {
-            create_circuit::<MNT4_753, _>(MNT6PKTreeNodeCircuit::rand(tree_level, rng), &filename);
-        }
-    }
+    // create_circuit::<MNT6_753, _>(MergerWrapperCircuit::rand(rng), "merger_wrapper");
+    // create_circuit::<MNT4_753, _>(MergerCircuit::rand(rng), "merger");
+    // create_circuit::<MNT6_753, _>(MacroBlockWrapperCircuit::rand(rng), "macro_block_wrapper");
+    // create_circuit::<MNT4_753, _>(MacroBlockCircuit::rand(rng), "macro_block");
+    // for tree_level in 0..5 {
+    //     let filename = format!("pk_tree_{}", tree_level);
+    //     if tree_level % 2 == 0 {
+    //         create_circuit::<MNT6_753, _>(MNT4PKTreeNodeCircuit::rand(tree_level, rng), &filename);
+    //     } else {
+    //         create_circuit::<MNT4_753, _>(MNT6PKTreeNodeCircuit::rand(tree_level, rng), &filename);
+    //     }
+    // }
     create_circuit::<MNT4_753, _>(PKTreeLeafCircuit::rand(rng), "pk_tree_5");
 }
 
